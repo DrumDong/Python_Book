bff2d9149e0749eaa5fb7c00e277b5d231b92d18
Donggyu Kim <rb9928@naver.com>
2022-05-15 19:30:09 +0900

    파이썬 패키지 로컬 설치방법

---
+++ Tech/Tech_02th.py
@@ -0,0 +1,10 @@
+'''
+1. setup.py 가 있는 폴더 로컬로 다운로드
+2. 해당 주소에서 python setup.py install 실행하면 설치 완료
+
+https://docs.python.org/ko/3/install/index.html
+'''
+
+import stemgraphic
+print(stemgraphic.__version__)
+
